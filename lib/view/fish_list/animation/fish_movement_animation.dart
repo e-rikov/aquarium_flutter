1153bed52c8b9183d07ea18595cef3a3e1e8f108
Евгений Риков <evgeniy.rikov@kupibilet.ru>
2020-05-17 09:03:27 +0300

    Remove redundant

--- lib/view/fish_list/animation/fish_movement_animation.dart
+++ lib/view/fish_list/animation/fish_movement_animation.dart
@@ -44,8 +44,8 @@ class FishMovementAnimation implements MovementAnimation {
 
     AnimationDescriptor _getNextAnimationDescriptor() {
         do {
-            _dx = _random.getNextNormalizedDouble();
-            _dy = _random.getNextNormalizedDouble();
+            _dx = _random.getNextNormalizedDouble;
+            _dy = _random.getNextNormalizedDouble;
         } while (_dx == 0.0 && _dy == 0.0);
 
         // Normalize direction:
@@ -58,8 +58,8 @@ class FishMovementAnimation implements MovementAnimation {
         final resultStartPoint = _currentAnimationDescriptor != null
             ? _currentAnimationDescriptor.endPoint
             : Point<double>(
-            _random.getNextNormalizedDouble() * 0.8,
-            _random.getNextNormalizedDouble() * 0.8);
+            _random.getNextNormalizedDouble * 0.8,
+            _random.getNextNormalizedDouble * 0.8);
 
         final resultEndPoint = getIntersectionWithUnitSquare(
             sx: resultStartPoint.x,
